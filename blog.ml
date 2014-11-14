7d231a5e9897efc7dee44ae3a465a8cb4b9da69f
ZAN DoYe <357626+kandu@users.noreply.github.com>
2014-11-14 23:07:59 +0800

    alpha

---
+++ blog.ml
@@ -0,0 +1,34 @@
+module StringSet = Set.Make(String)
+module StringMap = Map.Make(String)
+
+type lang= string
+type code= string
+type lineno= int option
+type uri= string
+
+type sentence=
+  | Raw of string
+  | Text of string
+  | Image of uri * string
+  | Mkd of string
+  | Code of lang * code * lineno
+  | Dot of string
+
+type paragraph= sentence list
+
+type article= {
+  title: string;
+  author: string;
+  date: string;
+  date_update: string option;
+  tags: StringSet.t;
+  head: paragraph list;
+  content: paragraph list;
+  tail: paragraph list;
+}
+
+type site=
+  { home: article;
+    tabs: article StringMap.t;
+    posts: article StringMap.t}
+
---
+++ html.ml
@@ -0,0 +1,55 @@
+module StringMap = Map.Make(String)
+
+let escape_query s=
+  Str.global_substitute
+    (Str.regexp "&")
+    (fun c-> "&amp;")
+    s
+
+let escape s=
+  Str.global_substitute
+    (Str.regexp "[ ><&=+\x00-\x1f]")
+    (function
+      | " " -> "+"
+      | c ->
+        Printf.sprintf "%%%02X" (int_of_char (Str.matched_string c).[0]))
+    s
+
+let unescape s=
+  Str.global_substitute
+    (Str.regexp "+\\|%[0-9a-fA-F][0-9a-fA-F]")
+    (function
+      | "+" -> "="
+      | c ->
+        String.sub (c |> Str.matched_string) 1 2
+        |> Printf.sprintf "0x%s"
+        |> int_of_string |> char_of_int |> String.make 1)
+    s
+
+let encode m=
+  StringMap.bindings m
+  |> List.map (fun (k,v)-> escape k ^ "=" ^ escape v)
+  |> String.concat "&"
+
+let decode s=
+  try
+    List.fold_left
+      (fun acc pair->
+        let kv= Str.(split (regexp "=") pair) in
+        StringMap.add (List.hd kv) (List.hd (List.tl kv)) acc)
+      StringMap.empty
+      Str.(split (regexp "&") s)
+  with _ -> StringMap.empty
+
+let head=
+{|<html>
+<head>
+    <link rel="stylesheet" href="/css/main.css" type="text/css"/>
+    <link rel="stylesheet" href="/css/markdown.css" type="text/css"/>
+    <link rel="stylesheet" href="/css/highlight.css" type="text/css"/>
+</head>
+<body>
+<div id="bg0"><div id="bg1">
+|}
+
+let tail= {|</div></div></body></html>|}
